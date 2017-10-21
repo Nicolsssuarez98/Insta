38632b34f944daea23d3f77a284cd548f47d1a00
Nicolsssuarez98 <lalala98teamo@gmail.com>
2017-10-21 13:06:06 -0500

    todo

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
+var gulp = require('gulp'),
+server = require('gulp-server-livereload'),
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
  
 gulp.task('nina', function() {
   gulp.src('./')
@@ -10,3 +14,23 @@ gulp.task('nina', function() {
       port: 9500
     }));
 });
+
+
+gulp.task('build-html', function () {
+  return gulp.src(['*.html', 'pa*/*.html'])
+      .pipe(useref())
+      .pipe(gulpif('*.js', uglify()))
+      .pipe(gulpif('*.css', minifyCss()))
+      .pipe(gulp.dest('build'));
+  });
+
+  
+gulp.task('build-assets-img', function () {
+  return gulp.src('asset*/**/**').
+    pipe(gulp.dest('build'))
+  })
+
+  
+gulp.task('build', ['build-html','build-assets-img'])
+
+  
--- script/main.js
+++ script/main.js
@@ -1,2 +1,17 @@
-$(".button-collapse").sideNav();
+var cont = 0
+  function mostrar(ele){
+    if(cont == 0){
+      ele.innerHTML = 'favorite'
+      ele.style.color ='red'
+      cont = 1
+    }
+    else{
+      ele.innerHTML = 'favorite_border'
+      ele.style.color ='black'
+      cont = 0
+    }
+  }
 
+  
+
+ 
